0106d3481b99b8100a431dd5a4c333dab441ccf0
patrick.rainer2@gmail.com <patrick.rainer2@gmail.com>
2019-07-28 20:08:45 +0200

    Main Menu and Resolution Settings

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // Prevent from going to Portrait mode
+        Screen.autorotateToPortrait = true;
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false; // TODO: Does error in build mode
+    }
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
---
+++ Assets/Scripts/ResolutionTester.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionTester : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            // Switch to 640 x 480 full-screen
+            Screen.SetResolution(640, 480, true);
+
+            Debug.Log("Key 1 pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            // Switch to 640 x 480 full-screen at 60 hz
+            Screen.SetResolution(640, 480, true, 60);
+
+            Debug.Log("Key 2 pressed");
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Resolution Change Keys 1 - 9");
+    }
+}
