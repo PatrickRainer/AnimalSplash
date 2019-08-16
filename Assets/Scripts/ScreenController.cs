20a006a68ea021341fc89dc7b8a5ed68a2a733f7
patrick.rainer2@gmail.com <patrick.rainer2@gmail.com>
2019-08-16 20:37:25 +0200

    Added ScreenController

---
+++ Assets/Scripts/ScreenController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The class controls all around the screen, like orientation, resolution, etc.
+/// </summary>
+public class ScreenController : MonoBehaviour
+{
+    void Start()
+    {
+        SetLandscapeOrientation();
+    }
+
+    private void SetLandscapeOrientation()
+    {
+        // Does prevent from AutoRotation
+        Screen.autorotateToPortraitUpsideDown = false;
+        Screen.orientation = ScreenOrientation.Landscape;
+    }
+
+}
