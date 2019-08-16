1b7e1d4f84f3e6d55d8ebd43324c6421488297dc
patrick.rainer2@gmail.com <patrick.rainer2@gmail.com>
2019-08-16 20:27:36 +0200

    Class LevelPrefs created
    Holds the Level Settings

---
+++ Assets/Scripts/LevelPrefs.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Holds the Preferences for this specific Level
+/// </summary>
+public class LevelPrefs : MonoBehaviour
+{
+    public GameObject[] animalPrefab;
+    public float levelDuration;
+    public float spawningDelay;
+
+}
