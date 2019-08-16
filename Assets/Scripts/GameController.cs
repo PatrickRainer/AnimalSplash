b054b00a890cf10bf495d757d5f0a3674a95fa93
patrick.rainer2@gmail.com <patrick.rainer2@gmail.com>
2019-08-16 20:12:00 +0200

    Created the GameController.cs
    - Singleton
    - Dont Destroy onLoad

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.MoveGameObjectToScene.html
+// https://docs.unity3d.com/ScriptReference/Object.DontDestroyOnLoad.html
+
+
+    /// <summary>
+    /// The Class controls the Main Elements of the whole game and the 
+    /// belonging GameObject, can be moved from scene to scene
+    /// </summary>
+public class GameController : MonoBehaviour
+{
+    #region SingletonPattern
+    private static GameController _instance;
+    public static GameController Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    #endregion
+
+}
